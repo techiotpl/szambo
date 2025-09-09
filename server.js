f1381975eb75cacb2481027e999781c54e5fab9e
techiotpl <biuro@techiot.pl>
2025-09-09 17:02:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use(helmet());
 app.set('trust proxy', 1);
 
 // ───────────────  REKLAMY (/ads)  ───────────────
-registerAdsRoute(app);
+registerAdsRoute(app, db);
 
 
 const PORT = process.env.PORT || 3000;
